83d32e9d51d9669784640659a1a5b4214b109029
GyPDigital <gypinformaticaytecnologia@gmail.com>
2022-09-15 17:58:59 -0500

    first commit

---
+++ src/components/Divisor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Divisor(){
+    return(
+        <div className="container-divisor">
+            <div className="divisor"></div>
+        </div>
+        
+    );
+}
+
+export default Divisor;
